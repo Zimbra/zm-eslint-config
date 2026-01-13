b027836f2b2bc021b1a5f8226e4d74bc30e85ce6
Damini Vashishtha <damini.vashishtha@synacor.com>
2026-01-13 16:31:56 +0530

    PREAPPS-8910 added few more rules

--- src/rules/style.js
+++ src/rules/style.js
@@ -1,4 +1,14 @@
 export const styleRules = {
+	'new-cap': 1,
+	'no-console': [1, { allow: ['warn', 'error'] }],
+	'no-shadow-restricted-names': 2,
+	'no-shadow': 'error',
+	eqeqeq: ['error', 'smart'],
+	camelcase: [1, { properties: 'never' }],
+	'guard-for-in': 2,
+	'prefer-rest-params': 2,
+	'no-alert': 'error',
+
 	// TODO: Fully enable this rule once the codebase is updated to follow best practices
 	'no-unused-vars': [
 		'error',
