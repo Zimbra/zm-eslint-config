5b8cc4400070f6a67477c223100916c691933f1f
Saket Patel <saket.patel@synacor.com>
2026-04-02 15:59:07 +0530

    PREAPPS-9055 Update documentation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import automationConfig from './configs/automation-config.js';
-import localeJsonConfig from './configs/locale-json-config.js';
 import coreJsConfig from './configs/core-js-config.js';
+import localeJsonConfig from './configs/locale-json-config.js';
+import automationConfig from './configs/automation-config.js';
+import prettierConfig from './configs/prettier-config.js';
 import customConfig from './configs/custom-config.js';
 import reactConfig from './configs/react-config.js';
 import preactI18nConfig from './configs/preact-i18n-config.js';
-import prettierConfig from './configs/prettier-config.js';
 
 export {
 	coreJsConfig,
