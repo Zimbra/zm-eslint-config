cf229ad7a05fbbdbb60f61a8e90a2c5d2ca2709b
miteshsavani810 <mitesh.savani@synacor.com>
2026-09-23 20:04:01 +0530

    PREAPPS-9718: Add preact-i18n translation JSON support and no-unsafe-template rule
    - Add 'preact-i18n/no-unsafe-template' rule to i18n rules configuration
    - Refactor locale-json-config to array format supporting multiple config objects
    - Add new config for handling preact-i18n translation JSON files (intl/, i18n/, translations/ paths)
    - Import fixupPluginRules from @eslint/compat for preact-i18n plugin compatibility
    
    PREAPPS-9718: Address code review

--- src/configs/locale-json-config.js
+++ src/configs/locale-json-config.js
@@ -1,20 +1,35 @@
+import { fixupPluginRules } from '@eslint/compat';
 import pluginI18nJson from 'eslint-plugin-i18n-json';
+import pluginPreactI18n from '@zimbra/eslint-plugin-preact-i18n';
+
 import { i18nJsonRules } from '../rules/i18n.js';
 
-export default {
-	files: ['**/*.json'],
+export default [
+	{
+		files: ['**/*.json'],
 
-	plugins: {
-		'i18n-json': pluginI18nJson
+		plugins: {
+			'i18n-json': pluginI18nJson
+		},
+
+		processor: {
+			meta: { name: '.json' },
+			...pluginI18nJson.processors['.json']
+		},
+
+		rules: {
+			...pluginI18nJson.configs.recommended.rules,
+			...i18nJsonRules
+		}
 	},
-
-	processor: {
-		meta: { name: '.json' },
-		...pluginI18nJson.processors['.json']
-	},
-
-	rules: {
-		...pluginI18nJson.configs.recommended.rules,
-		...i18nJsonRules
+	{
+		files: ['**/intl/**/*.json', '**/i18n/**/*.json', '**/translations/**/*.json'],
+		plugins: {
+			'preact-i18n': fixupPluginRules(pluginPreactI18n)
+		},
+		processor: 'preact-i18n/translation-json',
+		rules: {
+			'preact-i18n/no-unsafe-template': 'error'
+		}
 	}
-};
+];
